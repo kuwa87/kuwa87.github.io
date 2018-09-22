fc05dfc48fddb17d2c0d1bcef34be21abd01587a
kuwa87 <k.kuwamori8877@gmail.com>
2018-09-22 19:41:52 +0900

    delete

--- common/js/page-top.js
+++ common/js/page-top.js
@@ -1,70 +1,35 @@
 //page topボタン
-// jQuery(document).ready(function() {
-// var offset = 220;
-// var duration = 500;
-// jQuery(window).scroll(function() {
-// if (jQuery(this).scrollTop() > offset) {
-// jQuery('#page-top').fadeIn(duration);
-// } else {
-// jQuery('#page-top').fadeOut(duration);
-// }
-// });
-//
-// jQuery('#page-top').click(function(event) {
-// event.preventDefault();
-// jQuery('html, body').animate({scrollTop: 0}, duration);
-// return false;
-// })
-// });
-
-//
-// $(function(){
-// var topBtn=$('#page-top');
-// topBtn.hide();
-//
-//
-//
-// $(window).scroll(function(){
-//   if($(this).scrollTop()>80){
-//
-//     topBtn.fadeIn();
-//
-//   }else{
-//
-//     topBtn.fadeOut();
-//
-//   }
-// });
-//
-//
-//
-// topBtn.click(function(){
-//   $('body,html').animate({
-//   scrollTop: 0},500);
-//   return false;
-//
-// });
-//
-//
-// });
-
 
 $(function(){
-  $('#page-top').hide();                     // デフォルトで非表示とする
+var topBtn=$('#page-top');
+topBtn.hide();
 
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {          // ←【1】scrollTop() で現在のスクロール上の上位置を取得
-      $('#page-top').fadeIn();               // 100を過ぎていたら、#backToTop（戻るボタン）をフェードインさせる
-    } else {
-      $('#page-top').fadeOut();              // それ以外は、フェードアウト
-    }
-  });
 
-  // クリック時にスムーズにページ上部へスクロールさせる
-  $('#page-top').click(function(){
-    $('body,html').animate({ scrollTop: 0 }, 350);  // ←【2】
-    return false;  // ←【3】
-  });
- 
+
+//◇ボタンの表示設定
+$(window).scroll(function(){
+  if($(this).scrollTop()>80){
+
+    //---- 画面を80pxスクロールしたら、ボタンを表示する
+    topBtn.fadeIn();
+
+  }else{
+
+    //---- 画面が80pxより上なら、ボタンを表示しない
+    topBtn.fadeOut();
+
+  }
+});
+
+
+
+// ◇ボタンをクリックしたら、スクロールして上に戻る
+topBtn.click(function(){
+  $('body,html').animate({
+  scrollTop: 0},500);
+  return false;
+
+});
+
 
 });
