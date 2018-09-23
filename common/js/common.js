6d587c904420f33608cb4c374a5932f91d3d03a7
kuwa87 <k.kuwamori8877@gmail.com>
2018-09-23 23:04:36 +0900

    slider,etc

--- common/js/common.js
+++ common/js/common.js
@@ -18,26 +18,26 @@ $(function(){
   });
 
 });
-
-$(function() {
-  var $win = $(window),
-      $main = $('main'),
-      $nav = $('nav'),
-      navHeight = $nav.outerHeight(),
-      navPos = $nav.offset().top,
-      fixedClass = 'is-fixed';
-
-  $win.on('load scroll', function() {
-    var value = $(this).scrollTop();
-    if ( value > navPos ) {
-      $nav.addClass(fixedClass);
-      $main.css('margin-top', navHeight);
-    } else {
-      $nav.removeClass(fixedClass);
-      $main.css('margin-top', '0');
-    }
-  });
-});
+//header固定
+// $(function() {
+//   var $win = $(window),
+//       $main = $('main'),
+//       $nav = $('nav'),
+//       navHeight = $nav.outerHeight(),
+//       navPos = $nav.offset().top,
+//       fixedClass = 'is-fixed';
+//
+//   $win.on('load scroll', function() {
+//     var value = $(this).scrollTop();
+//     if ( value > navPos ) {
+//       $nav.addClass(fixedClass);
+//       $main.css('margin-top', navHeight);
+//     } else {
+//       $nav.removeClass(fixedClass);
+//       $main.css('margin-top', '0');
+//     }
+//   });
+// });
 
 
 // アンカーリンクはゆっくり
